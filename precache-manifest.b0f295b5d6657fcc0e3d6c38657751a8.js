fb8d53c9ee665195f765c84305a6db8b97845485
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-07-06 15:30:42 +0300

    Updates

---
+++ precache-manifest.b0f295b5d6657fcc0e3d6c38657751a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508b91bd81b14d6f521bc615b6628095",
+    "url": "/alQaisi/facerecognition/index.html"
+  },
+  {
+    "revision": "2426fb1f68349b556cd4",
+    "url": "/alQaisi/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d12c332065b8e72deba2",
+    "url": "/alQaisi/facerecognition/static/css/main.91874034.chunk.css"
+  },
+  {
+    "revision": "2426fb1f68349b556cd4",
+    "url": "/alQaisi/facerecognition/static/js/2.fb4ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alQaisi/facerecognition/static/js/2.fb4ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12c332065b8e72deba2",
+    "url": "/alQaisi/facerecognition/static/js/main.bd62bf5a.chunk.js"
+  },
+  {
+    "revision": "ec48a61027ae40439790",
+    "url": "/alQaisi/facerecognition/static/js/runtime-main.6a3452e7.js"
+  }
+]);
