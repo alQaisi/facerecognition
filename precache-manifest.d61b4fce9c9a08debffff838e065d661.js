b1886de955eda732a666dc4c0b4397d2af212532
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-07-06 15:40:11 +0300

    Updates

---
+++ precache-manifest.d61b4fce9c9a08debffff838e065d661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be5074ba077121a7e61361956734d1b",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "3439de92aa73c05b0347",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0adf67536744cadf3286",
+    "url": "/facerecognition/static/css/main.91874034.chunk.css"
+  },
+  {
+    "revision": "3439de92aa73c05b0347",
+    "url": "/facerecognition/static/js/2.1764066c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.1764066c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adf67536744cadf3286",
+    "url": "/facerecognition/static/js/main.3f2f7705.chunk.js"
+  },
+  {
+    "revision": "e457364e089452e68aa4",
+    "url": "/facerecognition/static/js/runtime-main.b0d2466f.js"
+  }
+]);
